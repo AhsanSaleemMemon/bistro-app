5a9e8c6cd0ff27e86d426ccfd3c0617ab01ebdb4
Ahsan Saleem <ahsansaleemmemon@gmail.com>
2021-10-07 15:33:10 +0500

    initial commit

---
+++ app/src/main/java/com/ahsansaleem/i170303_i170364/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ahsansaleem.i170303_i170364;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
