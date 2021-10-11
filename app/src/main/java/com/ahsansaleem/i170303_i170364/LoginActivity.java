4676c13d9c57f8a08388fe0244159cb46b2e6d08
aqeel1211 <aqeelejaz234@gmail.com>
2021-10-11 15:46:55 +0500

    added xmls

--- app/src/main/java/com/ahsansaleem/i170303_i170364/LoginActivity.java
+++ app/src/main/java/com/ahsansaleem/i170303_i170364/LoginActivity.java
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView sign_up;
+    Button log_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        log_in=findViewById(R.id.login_btn);
         sign_up = findViewById(R.id.sign_up_page);
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +26,15 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/ahsansaleem/i170303_i170364/MainActivity.java
+++ app/src/main/java/com/ahsansaleem/i170303_i170364/MainActivity.java
@@ -1,14 +1,44 @@
 package com.ahsansaleem.i170303_i170364;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Fragment {
+
+    // Add RecyclerView member
+    private RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        // Add the following lines to create RecyclerView
+        recyclerView = view.findViewById(R.id.rcv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RandomNumListAdapter(1234));
+
+        return view;
     }
 }
